2688b21ef4899d47fe4770bd30502e9869d88499
Tyler Hedrick <tylerhedrick@me.com>
2013-05-11 19:05:18 -0400

    including stephanies imageTiler into serialpm

--- serial_pm.cpp
+++ serial_pm.cpp
@@ -7,11 +7,31 @@ used in image matching for creating photo mosaics
 #include <math.h>
 #include <climits>
 #include <list>
+#include <jpeglib.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "mongo/client/dbclient.h"
 #include "CycleTimer.h"
 #include "imageSlicer.h"
 
 using namespace mongo;
+void add_images_to_raw(unsigned char* img, int index);
+
+/* pointer to new image */
+unsigned char *raw_image = NULL;
+
+/* mosaic tile image dimensions */
+int iwidth = 48;
+int iheight = 48;
+
+/* mosaic dimensions and component info */
+int width = 2448;
+int height = 2448;
+int bytes_per_pixel = 3;
+J_COLOR_SPACE color_space = JCS_RGB;
+
+/*** change in c++ to iwidth/width ***/
+int dim = width / iwidth;
 
 int square(int x) {
   return x * x;
@@ -35,14 +55,113 @@ int totalDistance(vector<RGB> a1, vector<RGB> a2) {
   return dist;
 }
 
+int read_jpeg_to_array(char *filename, int idx) {
+  /* standard libjpeg structures for reading */
+  struct jpeg_decompress_struct cinfo;
+  struct jpeg_error_mgr jerr;
+  /* structure to store scanline of image */
+  JSAMPROW row_pointer[1];
+  /* array to hold current image */
+  unsigned char *image_lines;
+  FILE *infile = fopen(filename, "rb");
+  unsigned long loc = 0;
+  int i = 0;
+
+  if (!infile) {
+    printf("Error opening file %s.\n", filename);
+    return -1;
+  }
+  /* set up all decompress and reading image */
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_decompress(&cinfo);
+  jpeg_stdio_src(&cinfo, infile);
+  jpeg_read_header(&cinfo, TRUE);
+  jpeg_start_decompress(&cinfo);
+
+  image_lines = (unsigned char*)malloc(cinfo.output_width*cinfo.output_height*cinfo.num_components);
+  row_pointer[0] = (unsigned char*)malloc(cinfo.output_width*cinfo.num_components);
+
+  /* writes image scanline info to image_lines */
+  while(cinfo.output_scanline < cinfo.image_height) {
+    jpeg_read_scanlines(&cinfo, row_pointer, 1);
+    for(i=0; i<(int)cinfo.image_width*cinfo.num_components; i++) {
+      image_lines[loc++] = row_pointer[0][i];
+    }
+  }
+
+  jpeg_finish_decompress(&cinfo);
+  jpeg_destroy_decompress(&cinfo);
+  free(row_pointer[0]);
+  fclose(infile);
+
+  /* write image to respective idx in mosaic array */
+  add_images_to_raw(image_lines, idx);
+  free(image_lines);
+  return 1;
+}
+
+void add_images_to_raw(unsigned char* img, int index) {
+  int j;
+  //int raw_loc = index*iwidth*iheight*bytes_per_pixel;
+  int start_loc = (index*iwidth*bytes_per_pixel) + ((width*(index/dim))*iheight*bytes_per_pixel);
+  int offset = (dim-1)*bytes_per_pixel;
+  int count = 0;
+  //printf("Image index %i\n", index);
+  for (j=0; j < iheight*iwidth*bytes_per_pixel; j++) {
+    if (count == iwidth*bytes_per_pixel) {
+      start_loc += iwidth*offset;
+      count = 0;
+    }
+    raw_image[start_loc++] = img[j];
+    count++;;
+  }
+}
+
+int write_jpeg_to_file(char *filename) {
+  struct jpeg_compress_struct cinfo;
+  struct jpeg_error_mgr jerr;
+  JSAMPROW row_pointer[1];
+  FILE *outfile = fopen(filename, "wb");
+
+  if (!outfile) {
+    printf("Error opening output file %s.\n", filename);
+    return -1;
+  }
+
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_compress(&cinfo);
+  jpeg_stdio_dest(&cinfo, outfile);
+
+  cinfo.image_width = width;
+  cinfo.image_height = height;
+  cinfo.input_components = bytes_per_pixel;
+  cinfo.in_color_space = color_space;
+
+  jpeg_set_defaults(&cinfo);
+  jpeg_start_compress(&cinfo, TRUE);
+
+  while(cinfo.next_scanline < cinfo.image_height) {
+    row_pointer[0] = &raw_image[cinfo.next_scanline*cinfo.image_width*cinfo.input_components];
+    jpeg_write_scanlines(&cinfo, row_pointer, 1);
+  }
+
+  jpeg_finish_compress(&cinfo);
+  jpeg_destroy_compress(&cinfo);
+  fclose(outfile);
+  return 1;
+}
+
 int main(int argc, char **argv) {
   InitializeMagick(0);
   if (argc < 4) {
     cout << "usage: " << argv[0] << " <image path> <save path> <cutSize>\n";
     return 1;
   }
+
+  raw_image = (unsigned char*)malloc(width*height*bytes_per_pixel*dim);
+
   int cutSize = atoi(argv[3]);
-  ImageSlicer slicer(argv[1], 102, cutSize);
+  ImageSlicer slicer(argv[1], 51, cutSize);
   string savepath = argv[2];
   DBClientConnection c;
   c.connect("localhost");
@@ -90,6 +209,7 @@ int main(int argc, char **argv) {
     for (int j = 0; j < (cutSize * cutSize); j++) {
       current.push_back(averages[i + j]);
     }
+    double subimagestart = CycleTimer::currentSeconds();
     for (size_t k = 0; k < dbImageColors.size(); k++) {
       // get distance from current subimage to current image from db.
       int dist = totalDistance(current, dbImageColors[k]);
@@ -98,10 +218,14 @@ int main(int argc, char **argv) {
         minVal = dist;
       }
     }
+    double subimageend = CycleTimer::currentSeconds();
+    if (i % 50 == 0) {
+      printf("Time to find one image match: %f\n", (subimageend - subimagestart));
+    }
     finalImages.push_back(dbImageSources[minIndex]);
     // replace minIndex with empty vector to remove values and avoid duplicates.
-    // vector<RGB> empty;
-    // dbImageColors[minIndex] = empty;
+    vector<RGB> empty;
+    dbImageColors[minIndex] = empty;
   }
   dbImageColors.resize(0);
   averages.resize(0);
@@ -118,12 +242,22 @@ int main(int argc, char **argv) {
     if (n % 100 == 0) {
       printf("Opening image %d at path %s\n", n, filename.c_str());
     }
-    Image mosaicImage(filename);
-    mosaicImage.resize("6x6");
-    images.push_back(mosaicImage);
+    // Image mosaicImage(filename);
+    // images.push_back(mosaicImage);
+    char* writable = new char[filename.size() + 1];
+    copy(filename.begin(), filename.end(), writable);
+    writable[filename.size()] = '\0';
+    read_jpeg_to_array(writable, n);
+    delete [] writable;
   }
-  montageImages(&finalMontage, images.begin(), images.end(), montage);
-  writeImages(finalMontage.begin(), finalMontage.end(), savepath);
+  // montageImages(&finalMontage, images.begin(), images.end(), montage);
+  // writeImages(finalMontage.begin(), finalMontage.end(), savepath);
+  char *writable = new char[savepath.size() + 1];
+  copy(savepath.begin(), savepath.end(), writable);
+  writable[savepath.size()] = '\0';
+  write_jpeg_to_file(writable);
+  delete [] writable;
+  free(raw_image);
   double montageend = CycleTimer::currentSeconds();
   printf("Time to create and write montage to file %f\n", (montageend - montagestart));
 }
