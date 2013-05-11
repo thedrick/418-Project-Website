046ba1eb5f23025ec5202d9d2c80273a8e557543
Stephanie Yeung <shatt3redx@yahoo.com>
2013-05-11 17:16:29 -0400

    just call read jpeg. commented stuff.

--- imageTiler.c
+++ imageTiler.c
@@ -5,34 +5,35 @@
 /* pointer to new image */
 unsigned char *raw_image = NULL;
 
+/* mosaic tile image dimensions */
 int iwidth = 612;
 int iheight = 612;
 
-int width = 2448;
-int height = 2448;
+/* mosaic dimensions and component info */
+int width = 4896;
+int height = 4896;
 int bytes_per_pixel = 3;
 int color_space = JCS_RGB;
 
-int dim = 2448/612;
+/*** change in c++ to iwidth/width ***/
+int dim = 4896/612;
 
-unsigned char* read_jpeg_to_array(char *filename) {
+int read_jpeg_to_array(char *filename, int idx) {
   /* standard libjpeg structures for reading */
   struct jpeg_decompress_struct cinfo;
   struct jpeg_error_mgr jerr;
   /* structure to store scanline of image */
   JSAMPROW row_pointer[1];
-
+  /* array to hold current image */
   unsigned char *image_lines;
-
   FILE *infile = fopen(filename, "rb");
   unsigned long loc = 0;
   int i = 0;
 
   if (!infile) {
     printf("Error opening file %s.\n", filename);
-    exit(1);
+    return -1;
   }
-
   /* set up all decompress and reading image */
   cinfo.err = jpeg_std_error(&jerr);
   jpeg_create_decompress(&cinfo);
@@ -43,31 +44,32 @@ unsigned char* read_jpeg_to_array(char *filename) {
   image_lines = (unsigned char*)malloc(cinfo.output_width*cinfo.output_height*cinfo.num_components);
   row_pointer[0] = (unsigned char*)malloc(cinfo.output_width*cinfo.num_components);
 
+  /* writes image scanline info to image_lines */
   while(cinfo.output_scanline < cinfo.image_height) {
     jpeg_read_scanlines(&cinfo, row_pointer, 1);
-
     for(i=0; i<cinfo.image_width*cinfo.num_components; i++) {
       image_lines[loc++] = row_pointer[0][i];
     }
-  } // close while loop
+  }
 
   jpeg_finish_decompress(&cinfo);
   jpeg_destroy_decompress(&cinfo);
   free(row_pointer[0]);
   fclose(infile);
 
-  return image_lines;
+  /* write image to respective idx in mosaic array */
+  add_images_to_raw(image_lines, idx);
+  free(image_lines);
+  return 1;
 }
 
 int add_images_to_raw(unsigned char* img, int index) {
   int j;
-  int start_loc;
+  //int raw_loc = index*iwidth*iheight*bytes_per_pixel;
+  int start_loc = (index*iwidth*bytes_per_pixel) + ((width*(index/dim))*iheight*bytes_per_pixel);
   int offset = (dim-1)*bytes_per_pixel;
-  printf("Image index %i\n", index);
-  //int raw_loc = index*iwidth*iheight*bytes_per_pixel; 
-
-  start_loc = (index*iwidth*bytes_per_pixel) + ((width*(index/dim))*iheight*bytes_per_pixel);
   int count = 0;
+  //printf("Image index %i\n", index);
   for (j=0; j < iheight*iwidth*bytes_per_pixel; j++) {
     if (count == iwidth*bytes_per_pixel) {
       start_loc += iwidth*offset;
@@ -81,9 +83,7 @@ int add_images_to_raw(unsigned char* img, int index) {
 int write_jpeg_to_file(char *filename) {
   struct jpeg_compress_struct cinfo;
   struct jpeg_error_mgr jerr;
-
   JSAMPROW row_pointer[1];
-
   FILE *outfile = fopen(filename, "wb");
 
   if (!outfile) {
@@ -111,12 +111,13 @@ int write_jpeg_to_file(char *filename) {
   jpeg_finish_compress(&cinfo);
   jpeg_destroy_compress(&cinfo);
   fclose(outfile);
-
   return 1;
 }
 
 int main() {
+  /* allocate final image space */
   raw_image = (unsigned char*)malloc(width*height*bytes_per_pixel*dim);
+
   char *filenames[16];
   filenames[0] = "one.jpg";
   filenames[1] = "two.jpg";
@@ -136,9 +137,16 @@ int main() {
   filenames[15] = "sixteen.jpg";
   char *outfilename = "testall.jpg";
   int i;
-  for (i=0; i<16; i++) {
+  /*for (i=0; i<16; i++) {
     add_images_to_raw(read_jpeg_to_array(filenames[i]), i);
-  }
+  }*/
+  read_jpeg_to_array(filenames[2], 2);
+  read_jpeg_to_array(filenames[5], 5);
+  read_jpeg_to_array(filenames[6], 6);
+  read_jpeg_to_array(filenames[3], 3);
+  read_jpeg_to_array(filenames[12], 12);
+
   write_jpeg_to_file(outfilename);
-  return 0;
+  free(raw_image);
+  return 1;
 }
